b87bb0ae8d7e07574dad343df7402a25276ef773
Okon Sylvester Charlie <silverprince4134u@gmail.com>
2024-02-08 15:11:27 +0100

    Made breaking changes

--- example/createRawTransaction/main.go
+++ example/createRawTransaction/main.go
@@ -1,145 +1,131 @@
 package main
 
-import (
-	"bytes"
-	"encoding/hex"
-	"fmt"
+// var (
+// 	mnemonic = "range sheriff try enroll deer over ten level bring display stamp recycle"
+// )
 
-	"github.com/billitech/go-hdwallet"
+// // utxo is an unspent transaction output of wallet
+// type utxo struct {
+// 	key    *hdwallet.Key
+// 	hash   string // hash of this utxo. example: f75f1b0810857dff6972767d745a57d0aeb77ae4845ef4f698a7bd50ec34a4b4 (BTC Testnet)
+// 	index  uint32 // index of this wallet at this utxo
+// 	amount int64  // satoshis amount of this utxo
+// }
 
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
-)
+// func (u *utxo) ToTxIn() (*wire.TxIn, error) {
+// 	hash, err := chainhash.NewHashFromStr(u.hash)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-var (
-	mnemonic = "range sheriff try enroll deer over ten level bring display stamp recycle"
-)
+// 	outPoint := wire.NewOutPoint(hash, u.index)
+// 	return wire.NewTxIn(outPoint, nil, nil), nil
+// }
 
-// utxo is an unspent transaction output of wallet
-type utxo struct {
-	key    *hdwallet.Key
-	hash   string // hash of this utxo. example: f75f1b0810857dff6972767d745a57d0aeb77ae4845ef4f698a7bd50ec34a4b4 (BTC Testnet)
-	index  uint32 // index of this wallet at this utxo
-	amount int64  // satoshis amount of this utxo
-}
+// func (u *utxo) Signature(tx *wire.MsgTx, index int) error {
+// 	address, err := u.key.AddressBTC()
+// 	if err != nil {
+// 		return err
+// 	}
 
-func (u *utxo) ToTxIn() (*wire.TxIn, error) {
-	hash, err := chainhash.NewHashFromStr(u.hash)
-	if err != nil {
-		return nil, err
-	}
+// 	addr, err := btcutil.DecodeAddress(address, u.key.Opt.Params)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	outPoint := wire.NewOutPoint(hash, u.index)
-	return wire.NewTxIn(outPoint, nil, nil), nil
-}
+// 	script, err := txscript.PayToAddrScript(addr)
+// 	if err != nil {
+// 		return err
+// 	}
 
-func (u *utxo) Signature(tx *wire.MsgTx, index int) error {
-	address, err := u.key.AddressBTC()
-	if err != nil {
-		return err
-	}
+// 	wif, err := btcutil.NewWIF(u.key.Private, u.key.Opt.Params, false)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	addr, err := btcutil.DecodeAddress(address, u.key.Opt.Params)
-	if err != nil {
-		return err
-	}
+// 	sig, err := txscript.SignatureScript(tx, index, script, txscript.SigHashAll, wif.PrivKey, false)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	script, err := txscript.PayToAddrScript(addr)
-	if err != nil {
-		return err
-	}
+// 	tx.TxIn[index].SignatureScript = sig
+// 	return nil
+// }
 
-	wif, err := btcutil.NewWIF(u.key.Private, u.key.Opt.Params, false)
-	if err != nil {
-		return err
-	}
+// // receiver is who and how much you want sent coins
+// type receiver struct {
+// 	net     *chaincfg.Params
+// 	address string // receiver address. example: mxLBntJAV4dF977jJjN6uhT9er9KKeMEgK (BTC Testnet)
+// 	amount  int64  // satoshis amount
+// }
 
-	sig, err := txscript.SignatureScript(tx, index, script, txscript.SigHashAll, wif.PrivKey, false)
-	if err != nil {
-		return err
-	}
+// func (r *receiver) ToTxOut() (*wire.TxOut, error) {
+// 	addr, err := btcutil.DecodeAddress(r.address, r.net)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	tx.TxIn[index].SignatureScript = sig
-	return nil
-}
+// 	script, err := txscript.PayToAddrScript(addr)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-// receiver is who and how much you want sent coins
-type receiver struct {
-	net     *chaincfg.Params
-	address string // receiver address. example: mxLBntJAV4dF977jJjN6uhT9er9KKeMEgK (BTC Testnet)
-	amount  int64  // satoshis amount
-}
+// 	return wire.NewTxOut(r.amount, script), nil
+// }
 
-func (r *receiver) ToTxOut() (*wire.TxOut, error) {
-	addr, err := btcutil.DecodeAddress(r.address, r.net)
-	if err != nil {
-		return nil, err
-	}
+// func main() {
+// 	master, err := hdwallet.NewKey(
+// 		hdwallet.Mnemonic(mnemonic),
+// 	)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	script, err := txscript.PayToAddrScript(addr)
-	if err != nil {
-		return nil, err
-	}
+// 	wallet, err := master.GetWallet(
+// 		hdwallet.CoinType(hdwallet.BTCTestnet),
+// 		hdwallet.AddressIndex(1),
+// 	)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	return wire.NewTxOut(r.amount, script), nil
-}
+// 	key := wallet.GetKey()
 
-func main() {
-	master, err := hdwallet.NewKey(
-		hdwallet.Mnemonic(mnemonic),
-	)
-	if err != nil {
-		panic(err)
-	}
+// 	senders := []*utxo{
+// 		{key: key, hash: "27b5ae231bc5a421b300095184b6b44c66f3ac3d3b6db1e7291a7bf27192946f", index: 0, amount: 990000},
+// 	}
 
-	wallet, err := master.GetWallet(
-		hdwallet.CoinType(hdwallet.BTCTestnet),
-		hdwallet.AddressIndex(1),
-	)
-	if err != nil {
-		panic(err)
-	}
+// 	receivers := []*receiver{
+// 		{net: key.Opt.Params, address: "mz5otQWaaPmWEydGw9gjfaHxSKhHX6Fgj5", amount: 990000},
+// 	}
 
-	key := wallet.GetKey()
+// 	tx := wire.NewMsgTx(wire.TxVersion)
 
-	senders := []*utxo{
-		{key: key, hash: "27b5ae231bc5a421b300095184b6b44c66f3ac3d3b6db1e7291a7bf27192946f", index: 0, amount: 990000},
-	}
+// 	for _, s := range senders {
+// 		in, err := s.ToTxIn()
+// 		if err != nil {
+// 			panic(err)
+// 		}
 
-	receivers := []*receiver{
-		{net: key.Opt.Params, address: "mz5otQWaaPmWEydGw9gjfaHxSKhHX6Fgj5", amount: 990000},
-	}
+// 		tx.AddTxIn(in)
+// 	}
 
-	tx := wire.NewMsgTx(wire.TxVersion)
+// 	for _, r := range receivers {
+// 		out, err := r.ToTxOut()
+// 		if err != nil {
+// 			panic(err)
+// 		}
 
-	for _, s := range senders {
-		in, err := s.ToTxIn()
-		if err != nil {
-			panic(err)
-		}
+// 		tx.AddTxOut(out)
+// 	}
 
-		tx.AddTxIn(in)
-	}
+// 	for i, s := range senders {
+// 		if err := s.Signature(tx, i); err != nil {
+// 			panic(err)
+// 		}
+// 	}
 
-	for _, r := range receivers {
-		out, err := r.ToTxOut()
-		if err != nil {
-			panic(err)
-		}
-
-		tx.AddTxOut(out)
-	}
-
-	for i, s := range senders {
-		if err := s.Signature(tx, i); err != nil {
-			panic(err)
-		}
-	}
-
-	buf := bytes.NewBuffer(make([]byte, 0, tx.SerializeSize()))
-	tx.Serialize(buf)
-	fmt.Println("Transaction:", hex.EncodeToString(buf.Bytes()))
-}
+// 	buf := bytes.NewBuffer(make([]byte, 0, tx.SerializeSize()))
+// 	tx.Serialize(buf)
+// 	fmt.Println("Transaction:", hex.EncodeToString(buf.Bytes()))
+// }
--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/billitech/go-hdwallet
 go 1.13
 
 require (
-	github.com/btcsuite/btcd v0.20.1-beta
+	github.com/btcsuite/btcd v0.24.0
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2
+	github.com/btcsuite/btcd/btcutil v1.1.5
 	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d
-	github.com/cpacia/bchutil v0.0.0-20181003130114-b126f6a35b6c
-	github.com/ethereum/go-ethereum v1.9.13
-	github.com/go-chain/go-tron v0.0.0-20191113113644-6938970e79d9
-	github.com/tyler-smith/go-bip39 v1.0.2
-	golang.org/x/crypto v0.0.0-20200427165652-729f1e841bcc // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	github.com/ethereum/go-ethereum v1.13.11
+	github.com/tyler-smith/go-bip39 v1.1.0
+	golang.org/x/crypto v0.17.0
 )
